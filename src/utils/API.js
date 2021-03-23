4dcdcdb3e828b5b7ecb90218a36574151f3b9807
Julie Ann <julieanniwinski@gmail.com>
2021-03-23 16:54:19 -0400

    Added API js

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,2 +1,10 @@
 import axios from "axios";
 
+
+const exportList = {
+    getEmployees: function() {
+        return axios.get("https://randomuser.me/api/?results=50&seed=foobar&exc=login");
+    }
+};
+
+export default exportList;
